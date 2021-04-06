87c3d69106a407b61864169705e55c69d788fda6
Afterflame <wither104@gmail.com>
2021-04-06 16:10:04 +1000

    Add files via upload

---
+++ App7/App7/AllColorsPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App7
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AllColorsPage : ContentPage
+    {
+        public AllColorsPage()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            ColorView.ItemsSource = NamedColor.All;
+        }
+    }
+}
---
+++ App7/App7/MainPage.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+
+namespace App7
+{
+    public class Notes
+    {
+        public string Text { get; set; }
+        public int Id { get; private set; }
+        private static int globalId = 0;
+
+        public Notes()
+        {
+            Id = ++globalId;
+        }
+    }
+
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+
+
+        public ICommand TapCommand
+        {
+            get
+            {
+                return new Command<string>((x) => ExecuteAction(x));
+            }
+        }
+        public void ExecuteAction(string x)
+        {
+            Console.WriteLine($"!{x}");
+        }
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindableLayout.SetItemsSource(Col1, Instance.marks1);
+            BindableLayout.SetItemsSource(Col2, Instance.marks2);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            button.IsEnabled = false;
+
+            var form = new Page1();
+            Navigation.PushAsync(form);
+            form.Disappearing += (send, ev) =>
+            {
+                button.IsEnabled = true;
+            };
+        }
+        private void Button_Col1_Clicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            button.IsEnabled = false;
+
+            var form = new ColorPage();
+            Navigation.PushAsync(form);
+            form.Disappearing += (send, ev) =>
+            {
+                button.IsEnabled = true;
+                this.Resources["GradientColor1"] = (Color)form.Resources["ChoosenColor"];
+                button.TextColor = new OppositeColor((Color)form.Resources["ChoosenColor"]).Color;
+            };
+        }
+        private void Button_Col2_Clicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            button.IsEnabled = false;
+
+            var form = new ColorPage();
+            Navigation.PushAsync(form);
+            form.Disappearing += (send, ev) =>
+            {
+                button.IsEnabled = true;
+                this.Resources["GradientColor2"] = (Color)form.Resources["ChoosenColor"];
+                button.TextColor = new OppositeColor((Color)form.Resources["ChoosenColor"]).Color;
+            };
+        }
+
+        private void Col1_SizeChanged(object sender, EventArgs e)
+        {
+            Instance.l1 = Col1.Height;
+        }
+
+        private void Col2_SizeChanged(object sender, EventArgs e)
+        {
+            Instance.l2 = Col2.Height;
+        }
+    }
+}
